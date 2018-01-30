64334425c1d3187b2b383308eebf3c912f20b62a
Laawe <laura.render.03@gmail.com>
2018-01-30 15:36:27 -0600

    1.- Botón desactivado y ya se añade contenido al html, aún no está 100 por ciento correcto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
 //Javascript//
-$(document).ready();
+$(document).ready(loadPage);
 
 //timeline-view//
 
+//traer elementos del html
+var $buttonPublish = $("#btn");
+var $inputUser = $("#text");
+
+function loadPage() {
+    $inputUser.keyup(ablePublish);
+}
+
+function ablePublish() {// con esta función se habilita el botón en caso de que el usuario ingrese algo
+    if ($("#text").val().trim().length === 0) {
+        $buttonPublish.attr("disabled", true);
+    } else {
+        $buttonPublish.removeAttr("disabled");
+        // se llama la función de click en el botón que agrega lo publicado por el usuario en el html
+        $buttonPublish.click(post);
+    }
+
+}
+
+function post() {
+    var $containerPost = $("#container-post");
+    var $input = $inputUser.val();
+    var $entry = $("<p />", {
+        "class": "new-boxes"
+    });
+    $entry.text($input);
+    $containerPost.prepend($input);
+    $("#publications").append($containerPost);
+    $buttonPublish.attr("disabled", true);
+
+
+
+}
